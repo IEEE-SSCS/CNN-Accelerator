6069abe59a402ab3e6c23e6d848b7520d8e6d18a
marwan ibrahim zaki <50636605+marwanibrahim546@users.noreply.github.com>
2021-09-18 19:24:03 +0200

    edited

--- rtl/Relu.sv
+++ rtl/Relu.sv
@@ -1,8 +1,8 @@
-module Relu #(parameter width)( 
-input logic  [width:0] in  ,   
-output logic [width:0] out 
+module Relu #(parameter width = 8)( 
+input logic  [width-1:0] in  ,   
+output logic [width-1:0] out 
 );
 
-assign out  =   ( in[width] == 0 ) ? in  : 0  ;
+assign out  =   ( in[width-1] == 0 ) ? in  : 0  ;
 
 endmodule 
