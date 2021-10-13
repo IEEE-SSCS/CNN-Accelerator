8c73ddb0f5cb7c52dd0cc521959287a4c6f94dff
Moustafa Darwish <e.moustafa2@gmail.com>
2021-10-13 15:30:51 +0200

    new branch for pooling unit

--- rtl/Add.sv
+++
@@ -1,9 +0,0 @@
-module Add
-  #(parameter width=8)(
-  input [width-1:0] a, b,
-  output [width-1:0] c
-  );
-  
-  assign c = a+b;
-  
-endmodule
--- rtl/Relu.sv
+++
@@ -1,8 +0,0 @@
-module Relu #(parameter width = 8)( 
-input logic  [width-1:0] in  ,   
-output logic [width-1:0] out 
-);
-
-assign out  =   ( in[width-1] == 0 ) ? in  : 0  ;
-
-endmodule 
