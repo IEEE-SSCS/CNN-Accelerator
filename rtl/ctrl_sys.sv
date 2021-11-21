0cb260169e908e1b5860b6acee502218cec64509
me98nna <82970046+me98nna@users.noreply.github.com>
2021-11-21 15:28:40 +0200

    Update ctrl_sys.sv

--- rtl/ctrl_sys.sv
+++ rtl/ctrl_sys.sv
@@ -2,7 +2,7 @@ module sys_ctrl (
 input clk,
 input w_ps,
 input rst,
-output reg ctrl_out,exit
+output reg ctrl_out,test
 );
 
 parameter s0 = 2'b00  ,
@@ -11,25 +11,25 @@ parameter s0 = 2'b00  ,
           
 
 logic [0:1] present,next;
-logic  x,y; 
+logic[3:0]  x,y; 
 
 always @(posedge clk or negedge rst) 
 	begin
-		if(rst) present <= s0;
-		else  begin  present <= next; x <= y; end 
+		if(rst) begin present <= s0; y <= 4'b0; end
+		else  begin  present <= next; y <= x; end 
 	end
 
 
 always @( present , w_ps )
 
 	begin 
-	 y = x;
+	 x = y;
 		next=s0;
 		case(present) 
-			s0: if(w_ps==1) begin next=s1; ctrl_out= 1; x=0; end
-			    else     begin next=s0 ; y=0; end   
+			s0: if(w_ps==1) begin next=s1; ctrl_out= 1;y = 4'b0; end
+			    else     begin next=s0 ;y = 4'b0; end   
                                                      // load to weight
-		    s1: if(w_ps==1 && y<32) begin  next=s1;  ctrl_out= 1;  y= x + 1;  end
+		        s1: if(w_ps==1 && x<4) begin  next=s1;  ctrl_out= 1;  x= y + 1; test=1; end
                                        // load to partial_sum
 			    else  begin  next=s2;  ctrl_out= 0; end 
 
