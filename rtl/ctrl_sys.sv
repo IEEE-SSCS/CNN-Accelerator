52b4b2a73a24bccf1c71b726efa8e8212d15d8b1
me98nna <82970046+me98nna@users.noreply.github.com>
2021-11-21 17:41:05 +0200

    Update ctrl_sys.sv

--- rtl/ctrl_sys.sv
+++ rtl/ctrl_sys.sv
@@ -13,23 +13,23 @@ parameter s0 = 2'b00  ,
 logic [0:1] present,next;
 logic[3:0]  x,y; 
 
-always @(posedge clk or negedge rst) 
+always_ff @(posedge clk or negedge rst) 
 	begin
 		if(rst) begin present <= s0; y <= 4'b0; end
 		else  begin  present <= next; y <= x; end 
 	end
 
 
-always @( present , w_ps )
+always_comb
 
 	begin 
 	 x = y;
-		next=s0;
+	 next=s0;
 		case(present) 
-			s0: if(w_ps==1) begin next=s1; ctrl_out= 1;y = 4'b0; end
-			    else     begin next=s0 ;y = 4'b0; end   
+			s0: if(w_ps==1) begin next=s1; ctrl_out= 1;x = 4'b0; end
+			    else     begin next=s0 ;x = 4'b0; end   
                                                      // load to weight
-		        s1: if(w_ps==1 && x<4) begin  next=s1;  ctrl_out= 1;  x= y + 1; test=1; end
+		        s1: if(w_ps==1 && x<4) begin  next=s1;  ctrl_out= 1;  x= y+ 1; test=1; end
                                        // load to partial_sum
 			    else  begin  next=s2;  ctrl_out= 0; end 
 
